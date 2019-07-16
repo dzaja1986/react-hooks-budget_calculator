efd0c4259cedfc9fdae40a00adcadebd851bad1a
Milan Miloradovic <milan.miloradovic@cybercom.com>
2019-07-16 10:06:46 +0200

    last commit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,11 @@ function App() {
   //single amount
   const [amount, setAmount] = useState("");
   //alert
-  const [alert, setAlert] = useState({show:false})
+  const [alert, setAlert] = useState({show:false});
+  //edit
+  const [edit, setEdit] = useState(false);
+  //etid item
+  const [id, setId] = useState(0);
   //*******************functionality**************************** */
   //handle charge
   const handleCharge = e => {
@@ -45,9 +49,19 @@ function App() {
     e.preventDefault();
     // console.log(charge, amount)
     if(charge !== '' && amount > 0) {
-     const singleExpense = {id:uuid(), charge, amount};
-     setExpenses([...expenses, singleExpense]);
-     handleAlert({type:'success', text:'item added'})
+     if(edit) {
+       let tempExpenses = expenses.map(item => {
+         return item.id === id ? {...item, charge, amount} : item;
+       });
+       setExpenses(tempExpenses);
+       setEdit(false);
+       handleAlert({type:'success', text:'item edited'})
+     }
+     else{
+      const singleExpense = {id:uuid(), charge, amount};
+      setExpenses([...expenses, singleExpense]);
+      handleAlert({type:'success', text:'item added'})
+     }
      setCharge("");
      setAmount("");
     }
@@ -57,6 +71,29 @@ function App() {
       text:'charge cant be empty value and amount value has to be bigger than zero'})
     }
     };
+    //clear all items
+    const clearItems = () => {
+      console.log('cleared all items');
+      setExpenses([]);
+      handleAlert({type: "danger", text: "all item deleted"});
+    };
+    //handle delete
+    const handleDelete = (id) => {
+      // console.log(`item deleted : ${id}`)
+      let tempExpenses = expenses.filter(item => item.id !== id);
+      setExpenses(tempExpenses);
+      handleAlert({type: "danger", text: "item deleted"});
+    }
+    //handle edit
+    const handleEdit = (id) => {
+    let expense = expenses.find(item => item.id === id);
+    let {charge, amount} = expense;
+    setCharge(charge);
+    setAmount(amount);
+    setEdit(true);
+    setId(id);
+    }
+
   return (
     <>
     {alert.show && <Alert type={alert.type} text={alert.text} />}
@@ -69,8 +106,10 @@ function App() {
           handleAmount={handleAmount}
           handleCharge={handleCharge}
           handleSubmit={handleSubmit}
+          edit={edit}
         />
-        <ExpenseList expenses={expenses} />
+        <ExpenseList expenses={expenses} handleDelete={handleDelete}
+        handleEdit={handleEdit} clearItems={clearItems} />
       </main>
       <h1>
         total spending :{" "}
